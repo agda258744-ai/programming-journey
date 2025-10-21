a4e61f9b29a1397f8635428c64210b008a59cb1f
agda258744-ai <agda258744@student.smkn1kandeman.sch.id>
2025-10-21 15:20:25 +0700

    Add files via upload

---
+++ Tugas 1 Fungsi/Tugas 1 Fungsi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1_Fungsi
+{
+    internal class Program
+    {
+        // Fungsi kali menerima tiga bilangan dan menampilkan hasil perkalian
+        static void kali(int a, int b, int c)
+        {
+            int hasil = a * b * c;
+            Console.WriteLine("hasil perkalian: " + hasil);
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan bilngan pertama: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan bilngan kedua: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan bilngan ketiga: ");
+            int z = int.Parse(Console.ReadLine());
+
+            kali(x, y, z); //Panggil fungsi
+        }
+
+
+    }
+}
---
+++ Tugas Fungsi 3/Tugas Fungsi 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Fungsi_3
+{
+    internal class Program
+    {
+        // Fungsi untuk mengkonversikan suhu dari Celcius ke Fahrenheit
+        static double KonversiSuhu(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+
+        static void Main(string[] args)
+        {
+            // Meminta input suhu dalam Celcius dari pengguna
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+
+            // Mengkonversi suhu menggunakan fungsi KonversiSuhu
+            double fahrenheit = KonversiSuhu(celcius);
+
+            // Menampilkan hasil konversi
+            Console.WriteLine($"{celcius}°C sama dengan {fahrenheit}°F");
+        }
+    }
+}
